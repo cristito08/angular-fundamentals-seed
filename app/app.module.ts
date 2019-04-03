98a751a82653d28717ec2b3e0649b2c3a1b5e7de
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2019-04-03 11:40:35 -0600

    two-way-binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
